728257d49b20b3b13c9f2b6790b458f9d6aa1e8a
sleek1004 <arojojoyeo@yahoo.com>
2022-09-02 23:44:30 +0100

    blackjack progress

--- index.js
+++ index.js
@@ -2,15 +2,61 @@ firstCard = 10
 secondCard = 13
 totalCard = firstCard + secondCard
 console.log(totalCard)
+let hasBlackJack = false
+// 1. Create a variable called isAlive and assign it to true
+let isAlive = true
 
-if (totalCard < 21){
+if (totalCard < 22){
     console.log("do you want to draw a new card?")
+    isAlive = false
 
 }
-else if (totalCard === 21){
+else if (totalCard === 23){
     console.log("wohoo! You've got blackjack!")
+    hasBlackJack = true
+} 
+
+// 2. Flip its value to false in the appropriate code block 
+
+else{
+    console.log("You're out of the game!")
+    
 }
 
-else if (totalCard > 21){
-    console.log("You're out of the game!")
+
+
+
+
+
+let  span = 21
+ if (span < 21){
+    console.log("You can not enter the club!")
+ } else{
+    console.log("you are welcome!")
+ }
+
+ console.log(hasBlackJack)
+
+ // 3. Log it out to check that you're doing it right
+ console.log(isAlive)
+
+
+
+
+ // Check if the person is elegible for a birthday card from the King! (100)
+
+let age = 100
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+
+if(age < 100){
+    console.log("not elegible")
+}else if(age === 100){
+    console.log("Here is your birthday card from the king!")
+}
+else{
+    console.log("Not elegible, you have already gotten one")
 }
